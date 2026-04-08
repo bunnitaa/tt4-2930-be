aea678d4aac3c22e4b4f2fbaf37123eda4827733
Renan Cavalcanti <renan_cavalcanti@hotmail.com>
2026-04-08 16:17:00 -0400

    task: force error backend

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const { initializeSocket } = require("./socket");
 
 const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
+const start
     try{
         await connectDB();
 
